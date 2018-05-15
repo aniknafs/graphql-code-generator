9ddf7e9cfbe1d8a91b914628217b0e3daa6d0680
Ali Niknafs <alinik@microsoft.com>
2018-05-15 12:34:01 -0700

    un-ignore /dist folders in each package

---
+++ packages/graphql-codegen-core/dist/operations/build-selection-set.d.ts
@@ -0,0 +1,8 @@
+import { SelectionSetItem } from '../types';
+import { GraphQLSchema, GraphQLType, SelectionSetNode } from 'graphql';
+export declare function separateSelectionSet(selectionSet: SelectionSetItem[]): any;
+export declare function buildSelectionSet(
+  schema: GraphQLSchema,
+  rootObject: GraphQLType,
+  node: SelectionSetNode
+): SelectionSetItem[];
